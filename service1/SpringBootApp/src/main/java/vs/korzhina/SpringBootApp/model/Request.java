5fbeb9342cd36daaa3a3b2dafa0320c6f97be576
korzhinav <korzhinav@example.com>
2024-10-20 00:55:08 +0500

    lr5

--- service1/SpringBootApp/src/main/java/vs/korzhina/SpringBootApp/model/Request.java
+++ service1/SpringBootApp/src/main/java/vs/korzhina/SpringBootApp/model/Request.java
@@ -13,39 +13,50 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Request {
 
+    //Уникальный идентификатор сообщения
     @NotBlank
     @Size(max=32)
     private String uid;
     
-    
+    //Уникальный идентификатор операции
     @NotBlank
     @Size(max=32)
     private String operationUid;
 
-
+    //Имя системы отправителя
     private String systemName;
 
+    //Время создания сообщения
     @NotBlank
     private String systemTime;
 
+    //Наименование ресурса
     private String source;
 
+    //Должность
     private Positions position;
 
+    //Зарплата
     private Double salary;
 
+    //Бонус
     private Double bonus;
 
+    //Количество рабочих часов
     private Integer workDays;
 
+    //Уникальный идентификатор коммуникации
     @Max(100000)
     @Min(1)
     private Integer communicationId;
 
+    //Уникальный идентификатор шаблона
     private Integer templateId;
 
+    //Код продукта
     private Integer productCode;
     
+    //Смс код
     private Integer smsCode;
 
     @Override
--- service1/SpringBootApp/src/main/java/vs/korzhina/SpringBootApp/model/Response.java
+++ service1/SpringBootApp/src/main/java/vs/korzhina/SpringBootApp/model/Response.java
@@ -6,12 +6,25 @@ import lombok.Data;
 @Data
 @Builder
 public class Response {
+    //Уникальный идентфикатор сообщения
     private String uid;
+
+    //Уникальный идентификатор операции
     private String operationUid;
+
+    //Время создания сообщения
     private String systemTime;
+
+    //Код ответа
     private Codes code;
+
+    //Годовой бонус
     private Double annualBonus;
+
+    //Код ошибки
     private ErrorCodes errorCode;
+
+    //Сообщение об ошибке
     private ErrorMessages errorMessage;
  
 }
